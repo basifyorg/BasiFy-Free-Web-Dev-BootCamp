82531f19afb867ceee227a93c6e9e878e9cebbb6
S M Abiduzzaman <98245641+abid2012@users.noreply.github.com>
2024-10-23 00:29:33 +0600

    Add files via upload

---
+++ JS/class01/Second/script.js
@@ -0,0 +1,4 @@
+const x=5;
+const y=2;
+const result=x+y;
+console.log("The result is: "+result);
---
+++ JS/class02/script.js
@@ -0,0 +1,192 @@
+// document.write("Basify");
+//alert("Basify");
+//console.log("Basify");
+
+// var x=5;
+// let y=10;
+// const z=50;
+
+// console.log(z);
+
+
+//let x=5; //number = int
+//let y=5.6; //number = float/double
+//let z="Hello"; //string
+
+//let a=true; //boolean
+
+//let o;
+//console.log(o);
+//alert(o);
+
+/*
+var x=3;
+var x=4; //redeclare re-update
+
+let u=10;
+u=20; //not redeclared, but re-update possible 
+
+const t=1;
+t=2; //not redeclared, not re-updatable
+*/
+
+
+//const x = Symbol("Basify");
+//console.log(x);
+//console.log(x.description);
+
+//Arithmatic operators + - * / ** %
+
+//let count=3;
+//count+=5;
+// count = count+5;
+// console.log(count);
+
+//let sub=10;
+// sub-=5;
+// console.log(sub);
+
+// let m=5;
+// m*=6;
+// console.log(m);
+
+// let x=10;
+// x/=2;
+// console.log(x);
+
+// let a = Symbol(10);
+// console.log(a.description);
+
+//conditional operator
+
+// let b=10;
+
+// if(b == 10){
+//     console.log("hello");
+// }
+// else{
+//     console.log("gelo");
+// }
+
+// let s="Halum";
+
+// if(s == "halum"){
+//     console.log(s);
+// }
+// else{
+//     console.log("Else part");
+// }
+
+
+// let n=20;
+
+// if(n == 10){
+//     console.log(n+10);
+// }
+
+// else if(n == 20){
+//     console.log(n*5);
+// }
+
+// else if(n == 20){
+//     console.log("N is 20");
+// }
+
+// else{
+//     console.log("Else part");
+// }
+
+//ambiguity = confused
+
+
+//comparison operator: == < > <= >= !=
+
+// let num=100;
+
+// if(num < 100){
+//     console.log("Num is less than 100");
+// }
+
+// else if(num > 100){
+//     console.log("Num is greater than 100");
+// }
+
+// else{
+//     console.log("This is else");
+// }
+
+
+// let n=120;
+
+// if(n>=50){
+//     console.log("N >= 50");
+// }
+// else if(n<50){
+//     console.log("N < 50");
+// }
+
+// else if(n>100){
+//     console.log("N > 100");
+// }
+// else{
+//     console.log("This is else");
+// }
+
+
+/*
+let o=10;
+
+if(o<=40){
+    let x=5;
+    let y=2;
+    let result=x+y; //7
+    console.log("The result is "+result);
+}
+else if(o!=50){ //o er moddhe 50 baade onno kisu ache kina
+    console.log("Not equal");
+}
+else{
+    console.log("Else");
+}
+*/
+
+//let x=10;
+//let y=2;
+
+
+//Logical operator = AND, OR, NOT
+/*
+if(x == 10 && y == 5){       //AND
+    console.log("Nice!");
+}
+else{
+    console.log("Bad");
+}
+*/
+
+/*
+if(x==10 || y==1){  //OR
+    console.log("Bah!");
+}
+else{
+    console.log("Sih!");
+}
+*/
+
+/*
+let x=5;
+
+if(!(x==1)){ 
+    console.log("Hi there!");
+}
+else{
+    console.log("Bye there!");
+}
+*/
+
+
+//Ternary operator (conditional)
+
+let x=100;
+//x==10 ? console.log("X is 10") : (x==5? console.log("hello"): (x==15)? console.log("15"): console.log("Else"))
+x==10 ? console.log("X is 10") : console.log("This is else"); console.log("aadgadg")
