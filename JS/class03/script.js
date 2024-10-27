59f257e2677d5a5fa1b3da55985f75cd7f1338f6
S M Abiduzzaman <98245641+abid2012@users.noreply.github.com>
2024-10-27 22:58:32 +0600

    Add files via upload

---
+++ JS/class03/script.js
@@ -0,0 +1,127 @@
+//Even= jor or Odd = bijor
+
+/*
+let n = 2020;
+
+if(n%2 == 0){ //2 diye vag korle vagshesh koto
+    console.log("Even");
+}
+else{
+    console.log("Odd");
+}            
+
+// if(n%2 == 1){
+//     console.log("Odd");
+// }
+// else{
+//     console.log("Even");
+// }
+
+*/
+
+/*
+let x = prompt("Enter a number: ");
+
+if(x%2 == 0){
+    console.log("Even");
+}
+else{
+    console.log("Odd");
+}
+*/
+
+/*
+for(let i=1; i<=20; i++){
+    console.log("Hello");
+}
+*/
+
+/*
+for(let i=0; i<5; i+=2){   //i=i+2
+    console.log("Hello")
+}
+*/
+
+/*
+for(let i=0; i<7; i++){
+    console.log("Basify")
+}
+*/
+
+/*
+for(let i=0; i<10; i+=2){
+    console.log(i);
+}
+*/
+
+/*
+for(let i=0; ;i++){ //infinity loop
+    
+    
+    if(i == 10){
+        break;
+    }
+
+    console.log(i)
+
+    
+}
+
+*/
+
+/*
+for(let i=0; i<5; i++){
+   
+    if(i == 3){
+        continue;
+    }
+
+    console.log(i);
+}
+*/
+
+/*
+for(let i=10; i>=0; i--){
+    console.log(i)
+}
+
+*/
+
+/*
+let i=10;
+while(i>0){
+    console.log(i);
+    i--;
+*/
+
+/*
+while(1){ //infinite loop
+    console.log("Hello")
+*/
+
+/*
+let i=1;
+do{
+    console.log(i);
+    i++;
+}
+while(i<10);
+*/
+
+/*
+for(i=20; i>0; i-=5){
+    console.log(i);
+}
+
+*/
+
+let car={
+    color: "Black",
+    brandName: "Toyota",
+    tire: 4
+}
+
+//console.log(car);
+console.log("Color = "+car.color);
+console.log("Brand Name = "+car.brandName);
+console.log("Tires = "+car.tire);
