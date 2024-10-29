55ece7a2bace32334c56a0ddd41b62ad0f38ad99
S M Abiduzzaman <98245641+abid2012@users.noreply.github.com>
2024-10-29 22:57:42 +0600

    Add files via upload

---
+++ JS/class04/script.js
@@ -0,0 +1,83 @@
+/*
+let x=[4,1,7];
+// console.log(x);
+console.log(x[0]);
+console.log(x[1]);
+console.log(x[2]);
+*/
+
+//index = 0
+//length = 1
+
+/*
+let arr=[5,1,2,10,4,28,57,36];
+
+for(let i=0; i<arr.length; i++){
+    
+    console.log(i+"-->"+arr[i]);
+}
+
+*/
+
+/*
+let mix = [6,"Hello",4.7,"Basify",true];
+
+for(let i=0; i<mix.length; i++){
+    console.log(mix[i]);
+}
+*/
+
+/*
+let twod = [[5,2,1],[6,7,8]];
+
+for(let i=0; i<2; i++){
+    for(let j=0; j<3; j++){
+        console.log(twod[i][j]);
+    }
+}
+*/
+
+/*
+let numbers=[6,2,1,3];
+
+for(let value of numbers){
+    console.log(value);
+}
+*/
+
+/*
+let obj = {
+    x: 1,
+    y: 5,
+    z: 2,
+    name: "Basify",
+    upcoming: "New Web App"
+}
+
+for(let key in obj){
+    console.log(key+": "+obj[key]);
+}
+*/
+
+/*
+let car={
+    brandName: "BMW",
+    color: "Black",
+    tire: 4,
+}
+
+for(let key in car){
+    console.log(key+": "+car[key]);
+}
+*/
+
+
+let test = [[10,20],[5,7]];
+
+for(let i=0; i<2; i++){
+    for(let j=0; j<2; j++){
+        console.log(test[i][j]);
+    }
+}
+
+
